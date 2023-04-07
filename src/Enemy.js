e3ca609ef9bd3e8270bcbff4d8d304b3f523d05c
Kendall Malik Howard <kendallhoward04@gmail.com>
2023-04-06 20:54:35 -0700

    Made the Enemies Move around the board

--- src/Enemy.js
+++ src/Enemy.js
@@ -1,3 +1,5 @@
+import MovingPacman from "./MovingPacman.js";
+
 export default class Enemy {
 constructor (x,y,gameSize, velocity, gameBoard) {
     this.x = x;
@@ -6,16 +8,82 @@ constructor (x,y,gameSize, velocity, gameBoard) {
     this.velocity = velocity;
     this.gameBoard = gameBoard;
 
+
     this.#loadImages();
+
+    this.movingDirection = Math.floor(
+        Math.random() * Object.keys(MovingPacman).length
+    );
+
+    this.directionTimerDefault = this.#random(10,50)
+    this.directionTimer = this.directionTimerDefault;
 }
 
 
     draw(ctx) {
+        this.#move();
+        this.#changeDirection();
         ctx.drawImage(this.image, 
             this.x, 
             this.y, 
             this.gameSize, 
             this.gameSize);
+    }
+
+#changeDirection() {
+    this.directionTimer --;
+    let newMoveDirection = null;
+    if(this.directionTimer == 0) {
+        this.directionTimer = this.directionTimerDefault;
+        newMoveDirection = Math.floor(
+            Math.random() * Object.keys(MovingPacman).length
+        );
+    }
+     if(newMoveDirection != null && this.movingDirection != newMoveDirection)  {
+        if(
+        Number.isInteger(this.x / this.gameSize) &&
+        Number.isInteger(this.y / this.gameSize)
+        ) {
+            if(
+                !this.gameBoard.wallCollision(
+                this.x, 
+                this.y, 
+                newMoveDirection
+                )
+                ) {
+                this.movingDirection = newMoveDirection;
+            }
+        }
+     }
+}
+
+
+#move() {
+    if(
+        !this.gameBoard.wallCollision(
+        this.x,
+        this.y,
+        this.movingDirection
+          )
+          ) {
+            switch(this.movingDirection) {
+                case MovingPacman.up:
+                this.y -= this.velocity;
+                break;
+                case MovingPacman.down:
+                this.y += this.velocity;
+                break;
+                case MovingPacman.left:
+                this.x -= this.velocity;
+                break;
+                case MovingPacman.right:
+                this.x += this.velocity;
+                break;
+            }
+}
+}
+    #random (min, max) {
+        return Math.floor(Math.random()* (max -min +1 )) + min;
     }
 
     #loadImages(){
